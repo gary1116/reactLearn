6667cf7fd7576aa5ad0d91b7dc8076109f8eeb0f
gary1116 <garygodwin234@gmail.com>
2026-02-27 08:24:36 +0530

    form input

--- reactlearning/src/App.jsx
+++ reactlearning/src/App.jsx
@@ -3,34 +3,78 @@ import { useState } from 'react';
 
 function App() {  
 
-  const [counters,setCounters]=useState([{id:1,value:0}]);
-  const addCounters=()=>{
-    setCounters(previousCounter=>[...previousCounter,{id:previousCounter.length+1,value:0}]);
+  const [formData,setFormData]=useState({
+    text:'',
+    checkBox:false,
+    radio:'',
+    select:''
+  });
+
+  const handleChange=(e)=>{
+
+    const {name,value,type,checked}=e.target;
+    setFormData({
+      ...formData,
+      [name]:type==='checkbox'? checked:value
+    })
+
   }
 
-const incrementCounter = (id) => {
-    setCounters(prev =>
-      prev.map(counter =>
-        counter.id === id
-          ? { ...counter, value: counter.value + 1 }
-          : counter
-      )
-    );
-  };
   return (
     <>
-    <div className='App'>
-      <button className='buttonStyle' onClick={addCounters}>Add Counter</button>
-      <ul>
-        {counters.map(counter=>{
-         return (
-          <li key={counter.id}>
-            Counter {counter.id}:{counter.value}
-            <button className='buttonStyle' onClick={()=>incrementCounter(counter.id)}>Increment</button>
-          </li>
-          );
-        })}
-      </ul>
+    <div className='form-container'>
+        <h1>Form</h1>
+        <form action="">
+          <div className="form-field">
+            <label htmlFor="">Text:</label>
+            <input 
+            type="text" 
+            name='text' 
+            value={formData.text} 
+            onChange={handleChange}/>
+          </div>
+           <div className="form-field">
+           <label htmlFor="">
+            <input type="checkbox" checked={formData.checkBox} name='checkBox' onChange={handleChange}/>
+            CheckBox
+           </label>
+          </div>
+          <div className="form-field">
+           <label htmlFor="">
+            Radio
+           </label>
+           <label htmlFor="">
+              <input type='radio' name='radio' value='option1'  checked={formData.radio==='option1'} onChange={handleChange}/>
+            Option 1
+            </label>
+          </div>
+          <div className="form-field">
+           <label htmlFor="">
+            Radio
+           </label>
+           <label htmlFor="">
+              <input type='radio' name='radio' value='option2'  checked={formData.radio==='option2'} onChange={handleChange}/>
+            Option 2
+            </label>
+          </div>
+          {/* DROP DOWN */}
+
+        <div className="form-field">
+            <label htmlFor="">Select:</label>
+            <select name="select" value={formData.select} onChange={handleChange}>
+              <option value="">-- Choose an Option --</option>
+              <option value="Option1">Option 1</option>
+              <option value="Option2">Option 2</option>
+            </select>
+        </div>
+        </form>
+        <h3>Form Data</h3>
+          <div className="form-data">
+          <p><strong>Text: </strong> {formData.text || 'N/A'}</p>
+          <p><strong>Text: </strong> {formData.checkBox ? 'Checked':'Unchecked'}</p>
+          <p><strong>Radio: </strong> {formData.radio || 'N/A'}</p>
+          <p><strong>Select: </strong> {formData.select || 'N/A'}</p>
+        </div>
     </div>
     </>
   )
