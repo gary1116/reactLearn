14b6ef94cbf5af238f598974d31c6499164d3a68
gary1116 <garygodwin234@gmail.com>
2026-02-28 09:09:07 +0530

    mouse tracker

--- reactlearning/src/App.jsx
+++ reactlearning/src/App.jsx
@@ -1,24 +1,14 @@
-import { useEffect, useState } from 'react';
 import './App.css'
-import Colopicker from './Colopicker';
+import MouseTracker from './MouseTracker';
 
 function App() {  
 
-  const [count,setCount]=useState(0);
-
-  useEffect(()=>{
-        document.title=`Count:${count+1}`;
-  },[count])
-
-  const incrementCount=()=>{
-    setCount(count+1);
-  }
+  
 
   return (
     <>
     <div className='App'>
-    <h1>UseEffect Hook</h1>
-    <button onClick={incrementCount}>Increment</button>
+      <MouseTracker />
     </div>
     </>
   )
