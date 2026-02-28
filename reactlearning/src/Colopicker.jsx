41bd974af7dc5d03233c07d4603a538fcf803eeb
gary1116 <garygodwin234@gmail.com>
2026-02-28 08:03:23 +0530

    made a color picker

---
+++ reactlearning/src/Colopicker.jsx
@@ -0,0 +1,35 @@
+import React, { useState } from 'react'
+
+const Colopicker = () => {
+  
+    const [backgroundColor, setBackgroundColor]=useState('#ffffff');
+
+    const colors=['#F54927','#F5276C','#C27AFF','#27D3F5','#F5B027','#4927F5','#27F5B0'];
+
+    const handleColorChange=(color)=>{
+        setBackgroundColor(color)
+    }
+
+    return (
+    <div style={{backgroundColor:backgroundColor,
+      minHeight: "100vh",
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "center",
+      justifyContent: "center"}}>
+      <h1>Colo Picker</h1>
+      <div className="color-pallete">
+        {colors.map((color,index)=>(
+            <div className="color-box" key={index} onClick={()=>handleColorChange(color)} style={{backgroundColor:color}}></div>
+        ))}
+      </div>
+
+        <div className="custom-color-picker">
+            <input type="color" name="" id="" value={backgroundColor} onChange={(e)=>handleColorChange(e.target.value)}/>
+        </div>
+
+    </div>
+  )
+}
+
+export default Colopicker
