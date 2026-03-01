fd07d65f31373d31c7d78b698da66ea0e0b11b01
gary1116 <garygodwin234@gmail.com>
2026-03-01 12:40:04 +0530

    removing time from dependency array

--- reactlearning/src/Apps/DigitalClock.jsx
+++ reactlearning/src/Apps/DigitalClock.jsx
@@ -1,18 +1,15 @@
-import React, { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 
 const DigitalClock = () => {
     const [time,setTime]=useState(new Date());
-
 
     useEffect(()=>{
 
        const timerId= setInterval(()=>{
                         setTime(new Date())
                         },1000)
-
         return ()=>clearInterval(timerId);
-
-    },[time]);
+    },[]);
 
   return (
     <div className='parentDiv'>
