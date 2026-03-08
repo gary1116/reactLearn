ce9ffa62dc36f3e7b183eab761bdaa76a4da49d5
gary1116 <garygodwin234@gmail.com>
2026-03-08 11:50:33 +0530

    more custom validations

--- reactlearning/src/Apps/ReactForms.jsx
+++ reactlearning/src/Apps/ReactForms.jsx
@@ -18,6 +18,13 @@ const ReactForms = () => {
         return true;
     }
 
+    const existingUserNames=['admin','user123','john']
+    const checkIfUserName= async (value)=>{
+        await new Promise(resolve=>setTimeout(resolve,1000));
+        return existingUserNames.includes(value);
+
+    }
+
     return (
         <div>
             <h1>Forms in React</h1>
@@ -26,7 +33,11 @@ const ReactForms = () => {
                     <input {...register('name',
                          { required: true,
                             validate:{validateName,
-                            isNotNumber:(value)=>isNaN(value)||'name should not be a number'
+                            isNotNumber:(value)=>isNaN(value)||'name should not be a number',
+                            checkUserName:async (value)=>{
+                                const exist = await checkIfUserName(value);
+                                return !exist || 'username already exists';
+                            }
                     }})}
                           />
                 </label>
@@ -38,6 +49,24 @@ const ReactForms = () => {
                          })} />
                 </label>
                 {errors.email && <p style={{ color: 'red' }}>{errors.email.message}</p>}
+                
+                <label >Password:
+                    <input type="password" {...register('password',
+                        { required: 'passwords are required bro',
+                        minLength:{value:2,message:'password should be greater than 2'}}
+                    )}/>
+                </label>
+                {errors.password && <p style={{ color: 'red' }}>{errors.password.message}</p>}
+
+                <label >Confirm Password:
+                    <input type="password" {...register('ConfirmPassword',
+                        { required: 'confirm the password!!',
+                        minLength:{value:2,message:'password should be greater than 2'},
+                     validate:value=>value===watch('password') || 'Passwords do not match'}
+                    )}/>
+                </label>
+                {errors.ConfirmPassword && <p style={{ color: 'red' }}>{errors.ConfirmPassword.message}</p>}
+                
                 <button className='button' type='submit' style={{ height: '30px', width: '80px' }}>Submit</button>
             </form>
         </div>
