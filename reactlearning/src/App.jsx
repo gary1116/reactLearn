7e4cbaf486ecd9f916296ab650e6548629cd11c0
gary1116 <garygodwin234@gmail.com>
2026-03-11 20:18:06 +0530

    dark mode toggling on  and off

--- reactlearning/src/App.jsx
+++ reactlearning/src/App.jsx
@@ -1,11 +1,12 @@
 import './App.css'
+import DarkMode from './Apps/DarkMode'
 import ReactForms from './Apps/ReactForms'
 import TailwindTesting from './Apps/tailwindTesting'
 
 function App() {  
   return (
     <>
-    <TailwindTesting />
+    <DarkMode/>
     </>
   )
 }
