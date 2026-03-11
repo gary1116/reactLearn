51f6fded3ed220505a550b377235eeb135dc93bc
gary1116 <garygodwin234@gmail.com>
2026-03-11 12:57:00 +0530

    learning tailwind

--- reactlearning/src/Apps/TailwindTesting.jsx
+++ reactlearning/src/Apps/TailwindTesting.jsx
@@ -2,14 +2,13 @@ import React from 'react'
 
 const TailwindTesting = () => {
   return (
-    <div className='bg-gray-100 flex justify-center items-center h-screen'>
-      <div className="bg-white p-16 rounded-lg shadow-lg ">
-        <h1 className='text-2xl font-bold '>
-            Hello, Tailwind CSS
-        </h1>
-        <p>simple example using Tailwind css</p>
-      </div>
-    </div>
+<div className="bg-gray-100 p-4">
+  <ul className="list-none md:flex md:gap-2">
+    <li className='bg-blue-500 text-white p-2 m-2 rounded-lg  md:px-4'>Item 1</li>
+    <li className="bg-blue-500 text-white p-2 m-2 rounded-lg md:px-4">Item 2</li>
+    <li className="bg-blue-500 text-white p-2 m-2 rounded-lg md:px-4">Item 3</li>
+  </ul>
+</div>
   )
 }
 
